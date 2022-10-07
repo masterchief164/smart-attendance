c826e0991f12b88d620502cef34ef52f9b674a76
YOGESH CHAND UPADHYAY <20bec126@iiitdmj.ac.in>
2022-10-08 00:13:09 +0530

    added Logout/connected Mongodb

---
+++ controllers/logout.controller.js
@@ -0,0 +1,15 @@
+
+const logout=(req,res)=>{
+    
+    
+    try {
+        res.clearCookie('token');
+        
+        res.status(200).json({success:true});
+    } catch (error) {
+        console.log(error);
+        res.status(500)
+            .send({error});
+    }
+}
+module.exports = {logout};
